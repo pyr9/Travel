3773d951de9a8c44490d7f2e45eeef0f58607dc7
panyuro <panyuro@163.com>
2022-11-16 22:14:08 +0800

    顶部轮播图 | 引入轮播图插件：vue-awesome-swiper https://github.com/surmon-china/vue-awesome-swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,21 @@
 <template>
-  <home-header></home-header>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+  </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/swiper'
 
 export default {
   name: 'Home',
   components: {
-    HomeHeader
-  }}
+    HomeHeader,
+    HomeSwiper
+  }
+}
 </script>
 
 <style scoped>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,22 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'carrousel',
+  data () {
+    return {
+      swiperOptions: {
+      }
+    }
+  }
+}
+</script>
