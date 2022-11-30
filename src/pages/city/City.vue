967f920b28b893863f59e9b134a7eb6e2cb2ad1e
panyuro <panyuro@163.com>
2022-11-30 14:31:14 +0800

    城市选择页 ｜ 添加搜索框
    注意点：box-sizing ：border-box 告诉浏览器：你想要设置的border和padding的值是包含在 width 内的

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './Header'
+import CitySearch from './search'
 
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped lang="stylus">
+@import "~styles/varibles.styl"
+.search
+  background-color $backgroundColor
+  height .72rem
+  padding 0 .1rem
+
+  .search-input
+    box-sizing border-box
+    width 100%
+    height .62rem
+    line-height .62rem
+    text-align center
+    border-radius .05rem
+    padding 0 .1rem
+    color #666
+</style>
