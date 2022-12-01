934e57326da802248b528c403a97e95ef115701c
panyuro <panyuro@163.com>
2022-12-01 20:01:38 +0800

    城市列表选择页 ｜ try catch 包装localStore，避免由于关闭了本地localStorage或者隐身模式导致的异常

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '上海'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (e) {
+}
+
 export default new Vuex.Store({
   state: {
-    city: localStorage.city || '上海'
+    city: defaultCity
   },
   mutations: {
     changeCity (state, city) {
       state.city = city
-      localStorage.city = city
+      try {
+        localStorage.city = city
+      } catch (e) {
+      }
     }
   }
 })
