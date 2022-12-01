876fbc9c9a8204118ef2c2810bf7bf15ef91e633
panyuro <panyuro@163.com>
2022-12-01 21:43:00 +0800

    城市列表选择页 ｜ Fix城市名称太长，无法正常显示 width -> min-width

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -21,33 +21,37 @@ export default {
 //@import "../../../assets/styles/varibles.styl"
 // css引入其他css,需要加波浪线
 @import "~styles/varibles.styl"
+.header
+  display: flex
+  background-color: $bgColor
+  height: $headerHeight
+  line-height: $headerHeight
+  color: white
 
-  .header
-    display: flex
-    background-color: $bgColor
-    height: $headerHeight
-    line-height: $headerHeight
-    color: white
   .header-left
     width: .64rem
-    float :left
+    float: left
     text-align center
     font-size .4rem
+
   .header-input
-    flex : 1
-    margin-top : .12rem
-    height :.62rem
-    line-height :.62rem
-    margin-left :.2rem
+    flex: 1
+    margin-top: .12rem
+    height: .62rem
+    line-height: .62rem
+    margin-left: .2rem
     padding-left .1rem
-    background-color :white
-    border-radius : .1rem
-    color :#ccc
+    background-color: white
+    border-radius: .1rem
+    color: #ccc
+
   .header-right
-    width: 1.24rem
-    float :right
+    min-width: 1.04rem
+    padding 0.1rem
+    float: right
     text-align: center
     color white
-    .icon
-      font-size .2rem
+
+  .icon
+    font-size .2rem
 </style>
