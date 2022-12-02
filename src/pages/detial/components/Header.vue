3f736ece94d9c4ee0a9129bd22ac6f34ab700457
panyuro <panyuro@163.com>
2022-12-02 15:24:00 +0800

    景点详情页 ｜ banner图片底部增加渐变色

--- src/pages/detial/components/Header.vue
+++ src/pages/detial/components/Header.vue
@@ -35,6 +35,7 @@ export default {
     color white
     line-height .6rem
     height .6rem
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
 
   .title
     flex 1
