6ee8a076e70b8acc8f7d6b1e7fc3a08088e15d7a
panyuro <panyuro@163.com>
2022-12-05 23:36:40 +0800

    景点详情页 ｜ header左侧增加圆形箭头，点击返回主页

---
+++ src/pages/detial/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+<router-link to="/" tag="div">
+  <div class="circle">
+    <span class="iconfont">&#xe624;</span>
+  </div>
+</router-link>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="stylus">
+  .circle
+    width .6rem
+    height .6rem
+    line-height .6rem
+    background-color #333333
+    position absolute
+    top .2rem
+    left .2rem
+    border-radius .4rem
+    color white
+    font-size .4rem
+    text-align center
+</style>
