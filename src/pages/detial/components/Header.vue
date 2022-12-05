5b293df61fe1f6875a333e4361abddbc7f45d108
panyuro <panyuro@163.com>
2022-12-05 23:52:18 +0800

    景点详情页 ｜ 当滚动的高度大于60时，显示圆形箭头，否则显示固定的横幅

--- src/pages/detial/components/Header.vue
+++ src/pages/detial/components/Header.vue
@@ -1,28 +1,60 @@
 <template>
-<router-link to="/" tag="div">
-  <div class="circle">
-    <span class="iconfont">&#xe624;</span>
+  <div>
+    <router-link v-show="showAbs" tag="div" to="/">
+      <div class="circle">
+        <span class="iconfont">&#xe624;</span>
+      </div>
+    </router-link>
+    <div v-show="!showAbs" class="fixed">
+      景点详情
+    </div>
   </div>
-</router-link>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data: function () {
+    return {
+      showAbs: true
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      this.showAbs = top <= 60
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 }
 </script>
 
-<style scoped lang="stylus">
-  .circle
-    width .6rem
-    height .6rem
-    line-height .6rem
-    background-color #333333
-    position absolute
-    top .2rem
-    left .2rem
-    border-radius .4rem
-    color white
-    font-size .4rem
-    text-align center
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+
+.circle
+  width .6rem
+  height .6rem
+  line-height .6rem
+  background-color #333333
+  position absolute
+  top .2rem
+  left .2rem
+  border-radius .4rem
+  color white
+  font-size .4rem
+  text-align center
+
+.fixed
+  background-color: $bgColor
+  height: $headerHeight
+  line-height: $headerHeight
+  color: white
+  text-align center
+  position fixed
+  top 0
+  left 0
+  right 0
 </style>
