29d3b7b4d75af92b11a3821f4e3fe2f9c6bc7222
panyuro <panyuro@163.com>
2022-12-08 22:06:16 +0800

    景点详情页 ｜ 使用递归组件，实现详情页列表

--- src/pages/detial/Detail.vue
+++ src/pages/detial/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :tickets="tickets"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,17 +10,44 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
+  data () {
+    return {
+      tickets: [
+        {
+          title: '成人票',
+          children: [{
+            title: '成人联票',
+            children: [
+              {title: '成人联票-1'},
+              {title: '成人联票-2'}
+            ]
+          },
+          {
+            title: '成人单项目票',
+            children: [
+              {title: '成人单项目票-1'},
+              {title: '成人单项目票-2'}
+            ]
+          }]
+        },
+        {title: '儿童票'}
+      ]
+    }
+  },
   components: {
+    DetailList,
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    List: DetailList
   }
 }
 </script>
 
-<style scoped lang="stylus">
- .content
-   height 900px
+<style lang="stylus" scoped>
+.content
+  height 900px
 </style>
---
+++ src/pages/detial/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="(item,index) in tickets" :key="index" class="border-bottom">
+      <span class="item">
+      {{ item.title }}
+      </span>
+      <div v-if="item.children" class="item-children">
+        <detail-list :tickets="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    tickets: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item
+  height .6rem
+  line-height .6rem
+
+.item-children
+  padding 0 .2rem
+</style>
