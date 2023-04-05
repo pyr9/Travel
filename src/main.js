7ad53d37c28cf6d5d0581d3fa582ef9eb7e31261
panyuro <panyuro@163.com>
2023-04-05 19:59:27 +0800

    老travel项目迁移
    1. 迁移所需要的依赖
    2. 迁移main.js
    3. 迁移styles
    4. 别名通过增加文件vue.config.js配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 项目的入口文件
+// 路由就是根据网址的不同，返回给用户不同的内容
 import {createApp} from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 项目的入口文件
+// 路由就是根据网址的不同，返回给用户不同的内容
+import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+fastClick.attach(document.body)
+
+createApp(App).use(store).use(router).use(VueAwesomeSwiper).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const {defineConfig} = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    chainWebpack: (config) => {
+        config.resolve.alias.set('styles', path.join(__dirname, './src/assets/styles'))
+    }
 })
